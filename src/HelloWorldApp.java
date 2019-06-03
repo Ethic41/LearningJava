1f648a5762bab8772c614125c1a5846bde2a96bf
ethic41 <dahirmuhammad3@gmail.com>
2019-06-03 16:44:59 +0100

    first commit

---
+++ src/HelloWorldApp.java
@@ -0,0 +1,15 @@
+/*
+* Author: Dahir Muhammad Dahir
+* Date: 03rd-June-2019 02:11 PM
+* About: my first app in java
+*
+*/
+
+
+public class HelloWorldApp {
+
+    public static void main(String[] args){
+
+        System.out.println("Hello World!");
+    }
+}
